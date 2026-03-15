a8911d7696300a7955c80e521e642e93dadaf5f2
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2026-03-15 22:34:42 +0530

    Create portfolio.tsx

---
+++ frontend/pages/portfolio.tsx
@@ -0,0 +1,15 @@
+import PortfolioTable from "../components/PortfolioTable"
+
+export default function Portfolio(){
+
+  return(
+
+    <div>
+
+      <h2>Your Portfolio</h2>
+
+      <PortfolioTable/>
+
+    </div>
+  )
+}
