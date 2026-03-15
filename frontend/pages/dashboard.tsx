97d07941e3ac0ec40aa58fd8dc2ec245637a10f1
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2026-03-15 22:33:48 +0530

    Create dashboard.tsx

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar"
+
+export default function Dashboard() {
+
+  return (
+    <div>
+
+      <Navbar />
+
+      <h2>Trading Dashboard</h2>
+
+      <p>Overview of market and portfolio</p>
+
+    </div>
+  )
+}
