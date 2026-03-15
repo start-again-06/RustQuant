1cffb9c2a95e025f96872a240d243c8a906f4485
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2026-03-15 22:33:20 +0530

    Create index.tsx

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function Home() {
+
+  return (
+    <div>
+      <h1>FinTech AI Trading Platform</h1>
+
+      <Link href="/dashboard">Dashboard</Link>
+    </div>
+  )
+}
