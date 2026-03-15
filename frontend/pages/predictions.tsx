84731a278c164be3e049f4de8453b82068875af3
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2026-03-15 22:34:15 +0530

    Create predictions.tsx

---
+++ frontend/pages/predictions.tsx
@@ -0,0 +1,15 @@
+import PredictionChart from "../components/PredictionChart"
+
+export default function Predictions(){
+
+  return(
+
+    <div>
+
+      <h2>AI Predictions</h2>
+
+      <PredictionChart />
+
+    </div>
+  )
+}
